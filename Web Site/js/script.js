24a17497ce51837db7833278d7ee66ade8ad25b7
Ed Carlos <130385096+edtuors@users.noreply.github.com>
2024-02-02 13:56:26 -0300

    Add files via upload

---
+++ Web Site/js/script.js
@@ -0,0 +1,24 @@
+// add sticky navigation bar
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function() {
+    header.classList.toggle("sticky", window.scrollY > 0);
+})
+
+// activate the hamburger menu and mobile navigation
+const hamb = document.querySelector(".hamb");
+const nav = document.querySelector(".mobile-nav")
+
+hamb.addEventListener("click", function() {
+    this.classList.toggle("active");
+    nav.classList.toggle("slide");
+});
+// website scrollReveal effect
+const sr = ScrollReveal( {
+    distance: "85px",
+    duration: 2000,
+    reset: false,
+});
+
+sr.reveal(".hero .desc", {delay: 250, origin: "bottom"});
+sr.reveal(".newsletter .content", {delay: 250, origin: "bottom"});
